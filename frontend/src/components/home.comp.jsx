c8c68c18c40e446696c1670b37a5c8b3da7d0d67
kaushikkampli <kaushikkampli@gmail.com>
2021-06-27 21:01:37 +0530

    final build

--- frontend/src/components/home.comp.jsx
+++ frontend/src/components/home.comp.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Home() {
     return (
         <div>
-            <h3>Afraid of loosing money by investing in stocks? Get your feet wet in the virtual world!</h3>
+            <h3>Afraid of losing money by investing in stocks? Get your feet wet in the virtual world!</h3>
 
             <p>
                 Introducing Stocky! A completely gamified clone of the real ones. You will start off with 10000$ in your Wallet. 
